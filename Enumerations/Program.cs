e8dc841c4b80daf38b10661513d5e28661d9c39c
alfcastillo90 <cacr1990@gmail.com>
2018-04-29 01:22:06 -0300

    ejemplo de enumeradores

---
+++ Enumerations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enumerations
+{
+    class Program
+    {
+        enum Days {Sun,Mon,Tue,Wed,Thu,Fri,Sat};
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Enumerations!");
+            int x = (int)Days.Sun;
+            int y = (int)Days.Fri;
+            Console.WriteLine("Sun = {0}",x);
+            Console.WriteLine("Fri = {0}",y);
+            Console.WriteLine(Days.Thu);
+
+        }
+    }
+}
---
+++ Structs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataStructures
+{
+    struct Car{
+        public string Make;
+        public string Model;
+        public string Color;
+        public void Accelerate(){
+            Console.WriteLine("Ruuuuun");
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Chevy";
+            myCar.Model = "Silverado";
+            myCar.Color = "Gray";
+            Console.WriteLine($"My car is a {myCar.Make} {myCar.Model} that is {myCar.Color} in color.");
+            Console.WriteLine();
+            myCar.Accelerate();
+
+        }
+    }
+}
