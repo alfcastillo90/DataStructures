dbc1d76aac2b092979378d839da5711c6bcb98ec
alfcastillo90 <cacr1990@gmail.com>
2018-04-29 17:36:07 -0300

    Correcciones y dos nuevos ejemplos Hastable y una coleccion secnilla

---
+++ HashTables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+namespace HashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Hashtable openWith = new Hashtable();
+            openWith.Add("txt","nodepad.exe");
+            openWith.Add(".cs","Visual Studio");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("dib", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+            try{
+                openWith.Add("txt","nodepad.exe");
+                 Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+                openWith["rtf"] = "winword.exe";
+                 Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+                if(!openWith.ContainsKey("ht")){
+                    openWith.Add("ht","htProgram.exe");
+                    Console.WriteLine("Value added for key = \"ht\": {0}", openWith["ht"]);
+                }
+               Console.WriteLine();
+                foreach( DictionaryEntry de in openWith )
+                {
+                    Console.WriteLine("Key = {0}, Value = {1}", de.Key, de.Value);
+                }
+            }
+            catch{
+                Console.WriteLine("An element with Key = txt already exists");
+            }
+        }
+    }
+}
---
+++ StandardCollections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+namespace StandardCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myArrList = new ArrayList();
+            myArrList.Add("First Item");
+            myArrList.Add("Third Item");
+            myArrList.Add("Second Item");
+
+            myArrList.Sort();
+            int itemIndex = myArrList.IndexOf("Third Item");
+
+            foreach(object obj in myArrList)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Third Item is at index {itemIndex}.");
+        }
+    }
+}
