f2bbc58961734723cf5ef37289270a4fc4b413b2
alfcastillo90 <cacr1990@gmail.com>
2018-04-29 01:41:10 -0300

    un ejemplo de un arreglo sencillo iterado con ciclos for y foreach

---
+++ singleArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace singleArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 4;           
+            numbers[1] = 5;
+            numbers[2] = 6;
+            numbers[3] = 7;
+            numbers[4] = 8;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(numbers[2]);
+            int total = 0;
+            for(int i = 0; i<numbers.Length;i++ ){
+                total = total + numbers[i];
+            }
+            Console.WriteLine(total.ToString());
+            int forEachTotal = 0;
+            foreach(int num in numbers)
+            {
+                forEachTotal = forEachTotal + num;
+            }
+            Console.WriteLine(forEachTotal);
+        }
+    }
+}
