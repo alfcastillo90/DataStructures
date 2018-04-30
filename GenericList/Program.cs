729d9895a05ce868a35b3247ed93827a9bd86ff1
alfcastillo90 <cacr1990@gmail.com>
2018-04-29 22:25:23 -0300

    Ejemplo de una lista

---
+++ GenericList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace GenericList
+{
+    class Student{
+        
+        public Student(string first, string last, int age, string prog)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.Age = age;
+            this.Program = prog;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Program { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            Student stud1 = new Student("Tom", "Thumb", 12, "Computer Science");
+            Student stud2 = new Student("Fred", "Flintstone", 45, "Geology");
+            Student stud3 = new Student("Mickey", "Mouse", 35, "Animation");
+            students.Add(stud1);
+            students.Add(stud2);
+            students.Add(stud3);
+            foreach(Student stud in students){
+                Console.WriteLine(stud.FirstName);
+            }
+        }
+    }
+}
